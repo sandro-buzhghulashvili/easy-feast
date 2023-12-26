3f55e37e040132d6c64cfc5d91c11db8c0ef915e
sandro <sbuzhghulashvili@gmail.com>
2023-12-26 17:39:33 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h2 className='text-red-700'>Hello</h2>
+
+      <h1 className='text-3xl text-blue-200 font-bold underline'>
+        Hello world!!!
+      </h1>
+    </>
+  );
+}
+
+export default App;
