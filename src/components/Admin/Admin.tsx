c6d2f9ebc2eadd3cb0eff1896aaf0681daf28c1b
sandro <sbuzhghulashvili@gmail.com>
2024-01-20 14:54:23 +0400

    production ready version 1

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import Dashboard from './Dashboard';
-import OrderChart from './Chart';
-import Menus from './Menus';
-import Orders from './Orders';
+import React, { useContext } from "react";
+import Dashboard from "./Dashboard";
+import OrderChart from "./Chart";
+import Menus from "./Menus";
+import Orders from "./Orders";
+import { useNavigate } from "react-router-dom";
+import { userContext } from "../../store/user-context";
 
 interface OrderEntry {
   month: string;
@@ -17,10 +19,17 @@ const AdminComponent: React.FC<{
   addons: any;
 }> = ({ usersData, menuData, orders, addons }) => {
   const orderData = Object.values(orders)[0] as OrderEntry[];
+  const navigate = useNavigate();
+  const ctx = useContext(userContext);
 
   const totalRevenue: number = orderData.reduce((a, b: any) => {
     return a + b.monthlyRevenue;
   }, 0);
+
+  const logoutHandler = () => {
+    ctx.logout();
+    navigate("/");
+  };
 
   return (
     <div>
@@ -32,7 +41,10 @@ const AdminComponent: React.FC<{
       <OrderChart orderData={orderData} />
       <Menus menu={Object.values(menuData)} addons={Object.values(addons)} />
       <Orders users={usersData} />
-      <button className="mx-auto mb-10 font-bold block px-7 py-2 text-xl bg-primary_orange text-white rounded-xl md:hover:scale-110 duration-300">
+      <button
+        onClick={logoutHandler}
+        className='mx-auto mb-10 font-bold block px-7 py-2 text-xl bg-primary_orange text-white rounded-xl md:hover:scale-110 duration-300'
+      >
         Logout
       </button>
     </div>
